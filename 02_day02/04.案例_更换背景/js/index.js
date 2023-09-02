1edc8eb8579ac4960822e5e90388505d643feb20
Robert-J-WANG <robin.nz420@gmail.com>
2023-09-02 13:05:33 +1200

    update background

--- 02_day02/04.案例_更换背景/js/index.js
+++ 02_day02/04.案例_更换背景/js/index.js
@@ -4,3 +4,25 @@
  *  2. 上传成功时，"保存"图片url网址
  *  3. 网页运行后，"获取"url网址使用
  * */
+
+document.querySelector(".bg-ipt").addEventListener("change", (e) => {
+  const df = new FormData();
+  df.append("img", e.target.files[0]);
+  // 1. 选择图片上传，设置body背景
+  axios({
+    url: "http://hmajax.itheima.net/api/uploadimg",
+    method: "POST",
+    data: df,
+  }).then((res) => {
+    const imgUrl = res.data.data.url;
+    // console.log(imgUrl);
+    document.body.style.backgroundImage = `url(${imgUrl})`;
+
+    // 2. 上传成功时，"保存"图片url网址
+    localStorage.setItem("imgUrl", imgUrl);
+  });
+});
+
+// 3. 网页运行后，"获取"url网址使用
+const imgUrl = localStorage.getItem("imgUrl");
+imgUrl && (document.body.style.backgroundImage = `url(${imgUrl})`);
